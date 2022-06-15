1879d945a999610ccc15a43542204a069a4a17fa
Fahmid Ovi <fahmidovi9@gmail.com>
2022-06-15 02:08:40 -0400

    Split movie-list controller and router

---
+++ .history/Server/Routes/movie-list_20220615020357.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+
+import {DisplayMovieList} from '../Controllers/movie-list';
+ 
+/* GET movie-list page */
+router.get('/movie-list', DisplayMovieList);
+
+export default router;
