a9adf1021a38ad1996d5ce69e7659ddffe9655df
Fahmid Ovi <fahmidovi9@gmail.com>
2022-06-16 22:34:58 -0400

    Enabled login and register page

---
+++ .history/Server/Controllers/auth_20220616221836.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import user from '../Models/user';
+import passport from 'passport';
+
+// Display Functions
+export function DisplayLoginPage(req : express.Request, res : express.Response, next : express.NextFunction) 
+{
+    res.render('index', {title: 'Login', page: 'login', message: req.flash('loginMessage'), displayName: ''});
+};
+
+export function DisplayRegisterPage(req : express.Request, res : express.Response, next : express.NextFunction) 
+{
+    res.render('index', {title: 'Register', page: 'home', message: req.flash('registerMessage'), displayName: ''});
+}
+
+// Processing Functions
+export function ProcessLoginPage(req : express.Request, res : express.Response, next : express.NextFunction) 
+{
+}
+
+export function ProcessRegisterPage(req : express.Request, res : express.Response, next : express.NextFunction) 
+{
+}
+
+export function ProcessLogoutPage(req : express.Request, res : express.Response, next : express.NextFunction) 
+{
+};
