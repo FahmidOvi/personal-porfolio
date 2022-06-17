a1148e3146baf5b8f22fe69021043a2acb227e39
Fahmid Ovi <fahmidovi9@gmail.com>
2022-06-17 00:11:06 -0400

    Fixed logout bug

--- .history/Server/Controllers/auth_20220617000545.ts
+++ .history/Server/Controllers/auth_20220617000545.ts
@@ -72,13 +72,8 @@ export function ProcessRegisterPage(req : express.Request, res : express.Respons
 
 export function ProcessLogoutPage(req : express.Request, res : express.Response, next : express.NextFunction) 
 {
-    req.logOut(function(err)
+    req.logOut(function()
     {
-        if (err)
-        {
-            console.error(err);
-            res.end(err);
-        }
         console.log('User Logged Out');
     });
     res.redirect('/login');
