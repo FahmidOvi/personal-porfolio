db6ebec78a47ad75b33b62a94fef86dcde632509
Fahmid Ovi <fahmidovi9@gmail.com>
2022-06-16 23:06:50 -0400

    Modified navbar for authentication

--- .history/Server/Controllers/index_20220616223004.js
+++ .history/Server/Controllers/index_20220616223004.js
@@ -2,24 +2,24 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DisplayContactPage = exports.DisplayServicesPage = exports.DisplayProjectsPage = exports.DisplayAboutPage = exports.DisplayHomePage = void 0;
 function DisplayHomePage(req, res, next) {
-    res.render('index', { title: 'Home', page: 'home', displayName: '' });
+    res.render('index', { title: 'Home', page: 'home' });
 }
 exports.DisplayHomePage = DisplayHomePage;
 ;
 function DisplayAboutPage(req, res, next) {
-    res.render('index', { title: 'About', page: 'about', displayName: '' });
+    res.render('index', { title: 'About', page: 'about' });
 }
 exports.DisplayAboutPage = DisplayAboutPage;
 function DisplayProjectsPage(req, res, next) {
-    res.render('index', { title: 'Projects', page: 'projects', displayName: '' });
+    res.render('index', { title: 'Projects', page: 'projects' });
 }
 exports.DisplayProjectsPage = DisplayProjectsPage;
 function DisplayServicesPage(req, res, next) {
-    res.render('index', { title: 'Services', page: 'services', displayName: '' });
+    res.render('index', { title: 'Services', page: 'services' });
 }
 exports.DisplayServicesPage = DisplayServicesPage;
 function DisplayContactPage(req, res, next) {
-    res.render('index', { title: 'Contact', page: 'contact', displayName: '' });
+    res.render('index', { title: 'Contact', page: 'contact' });
 }
 exports.DisplayContactPage = DisplayContactPage;
 ;
