b5772330e8d72056776572cced09a9869c91b73b
Fahmid Ovi <fahmidovi9@gmail.com>
2022-06-17 01:14:28 -0400

    Added type any in RemoteURI

--- .history/Server/Config/db_20220617011317.ts
+++ .history/Server/Config/db_20220617011317.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://127.0.0.1/media";
-export const RemoteURI = process.env.RemoteURI as any;
+export const RemoteURI = process.env.RemoteURI as String;
 export const HostName = "MongoDB Atlas";
 export const Secret = "someSecret";
