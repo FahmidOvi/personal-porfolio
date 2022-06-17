c81be975f8958a7e4abc1c26cb58b025147169e4
Fahmid Ovi <fahmidovi9@gmail.com>
2022-06-17 01:02:20 -0400

    Moved URI info to environment variable

--- .history/Server/Config/db_20220617010049.ts
+++ .history/Server/Config/db_20220617010049.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://127.0.0.1/media";
-export const RemoteURI = process.env.RemoteURI;
+export const RemoteURI = "mongodb+srv://fahmid:JKxsPWCrvhrwl2ro@cluster0.xv8y4k9.mongodb.net/media?retryWrites=true&w=majority"
 export const HostName = "MongoDB Atlas";
 export const Secret = "someSecret";
--- Server/Config/db.js
+++ Server/Config/db.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Secret = exports.HostName = exports.remoteURI = exports.LocalURI = void 0;
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://127.0.0.1/media";
-exports.remoteURI = "mongodb+srv://fahmid:JKxsPWCrvhrwl2ro@cluster0.xv8y4k9.mongodb.net/media?retryWrites=true&w=majority";
+exports.RemoteURI = process.env.RemoteURI;
 exports.HostName = "MongoDB Atlas";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
