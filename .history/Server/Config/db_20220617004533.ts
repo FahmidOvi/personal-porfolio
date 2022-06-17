965388b33fda5ce0ca2b19dfef7dbfe0ab0ecca5
Fahmid Ovi <fahmidovi9@gmail.com>
2022-06-17 00:53:05 -0400

    Switched URI to remote

--- .history/Server/Config/db_20220617004533.ts
+++ .history/Server/Config/db_20220617004533.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://127.0.0.1/media";
-export const remoteURI = "mongodb+srv://fahmid:JKxsPWCrvhrwl2ro@cluster0.xv8y4k9.mongodb.net/media?retryWrites=true&w=majority"
+export const remoteURI = "mongodb+srv://fahmid:JKxsPWCrvhrwl2ro@cluster0.xv8y4k9.mongodb.net/?retryWrites=true&w=majority"
 export const HostName = "MongoDB Atlas";
 export const Secret = "someSecret";
--- Server/Config/db.js
+++ Server/Config/db.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Secret = exports.HostName = exports.LocalURI = void 0;
+exports.Secret = exports.HostName = exports.remoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://127.0.0.1/media";
-exports.HostName = "localhost";
+exports.remoteURI = "mongodb+srv://fahmid:JKxsPWCrvhrwl2ro@cluster0.xv8y4k9.mongodb.net/media?retryWrites=true&w=majority";
+exports.HostName = "MongoDB Atlas";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
